4b5e372522ef3a503826b47d7a363c6d0f6ed85f
Jonathas Garcia <jonathas-garcia>
2021-12-07 17:30:42 -0300

    Ajustes

--- src/main/java/br/com/jonathasgarcia/demosns/SnsController.java
+++ src/main/java/br/com/jonathasgarcia/demosns/SnsController.java
@@ -1,14 +1,14 @@
 package br.com.jonathasgarcia.demosns;
 
+import io.awspring.cloud.messaging.config.annotation.NotificationMessage;
+import io.awspring.cloud.messaging.config.annotation.NotificationSubject;
+import io.awspring.cloud.messaging.endpoint.NotificationStatus;
+import io.awspring.cloud.messaging.endpoint.annotation.NotificationMessageMapping;
+import io.awspring.cloud.messaging.endpoint.annotation.NotificationSubscriptionMapping;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cloud.aws.messaging.config.annotation.NotificationMessage;
-import org.springframework.cloud.aws.messaging.config.annotation.NotificationSubject;
-import org.springframework.cloud.aws.messaging.endpoint.NotificationStatus;
-import org.springframework.cloud.aws.messaging.endpoint.annotation.NotificationMessageMapping;
-import org.springframework.cloud.aws.messaging.endpoint.annotation.NotificationSubscriptionMapping;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -24,7 +24,7 @@ public class SnsController {
     }
 
     @NotificationSubscriptionMapping
-    public void confirmUnsubscribeMessage(@RequestBody NotificationStatus notificationStatus) {
+    public void confirmUnsubscribeMessage(NotificationStatus notificationStatus) {
         logger.info("Subscribed to Topic");
         notificationStatus.confirmSubscription();
     }
